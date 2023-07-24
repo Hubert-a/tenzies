5ea0cf0873b3367c34096a7ff81e959ff086f43a
Hubert-a <hubertamarfio@gmail.com>
2023-07-24 13:54:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from 'react'
 import Die from './components/Die'
 import {nanoid} from 'nanoid'
 import Confetti from 'react-confetti'
+import Timer from './components/Timer'
 export default function App(){
+    
     const [dice , setDice ]= React.useState(allNewDice())
     const [tenzies, setTenzies]= React.useState(false)
+    const [isActive, setIsActive] = React.useState(false)
+    const [isPaused, setIsPaused] = React.useState(true)
+    const [count, setCount] = React.useState(0)
+    const [time, setTime] = React.useState(0)
 
-    React.useEffect(()=>{
-        const allHeld = dice.every(die=>die.isHeld)
-        const firstValue = dice[0].value
-        const allSameValue = dice.every(die =>die.value === firstValue)
-        if(allHeld && allSameValue){
-            setTenzies(true)
-        }
-
-    },[dice])
+     
+    //generating new dice
     function allNewDice(){
         const newDice = []
         for(let i = 0; i<10;i++){
@@ -22,6 +21,7 @@ export default function App(){
         }
         return newDice
     }
+    //function to push new die values to array
     function generateNewDie(){
        return {
             value:Math.ceil(Math.random()*6),
@@ -29,30 +29,85 @@ export default function App(){
             id:nanoid()
         }
     }
+    // function to check if the game has been won or not 
+    React.useEffect(()=>{
+        const allHeld = dice.every(die=>die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die =>die.value === firstValue)
+        if(allHeld && allSameValue){
+            setTenzies(true)
+            handlePauseResume()
+        }
+        
+    },[dice])
+
+    React.useEffect(()=>{
+        let interval = null
+
+        if(isActive && isPaused===false){
+            interval = setInterval(()=>{
+                setTime(time=>time +10)
+
+            },10)
+        }else{
+            clearInterval(interval)
+        }
+        return ()=>{
+            clearInterval(interval)
+        }
+    },[isActive, isPaused])
+
+    const handleStart = ()=>{
+        setIsActive(true)
+        setIsPaused(false)
+    }
+    const handlePauseResume = () => {
+        setIsPaused(!isPaused);
+      };
+      
+      const handleReset = () => {
+        setIsActive(false);
+        setTime(0);
+      };
+
+
+
     function rollDice(){
+        setCount()
         if(!tenzies){
             setDice(oldDice => oldDice.map(die=>{
-                return die.isHeld ? die:generateNewDie()
+                return die.isHeld ? die : generateNewDie()
+
             }))
+            
         }else{
+            handleReset()
             setTenzies(false)
             setDice(allNewDice())
+
         }
     }
     const diceElements = dice.map(die=>(
     <Die
-    key= {die.id}
-    value={die.value}
-    isHeld={die.isHeld}
-    holdDice = {()=>holdDice(die.id)}
+        key= {die.id}
+        value={die.value}
+        isHeld={die.isHeld}
+        holdDice = {()=>holdDice(die.id)}
     />
     ))
-
+    // function to check if all dice of same number is  held
     function holdDice(id){
+        // Probably where to start counting from 
+        handleStart()
         setDice(prevDice=>prevDice.map(die=>{
             return die.id===id?{...die, isHeld:!die.isHeld}:die
         }))
+        
     }
+    
+    
+
+
     return(
         <main>
             {tenzies && <Confetti/>}
@@ -62,6 +117,12 @@ export default function App(){
                 {diceElements}
             </div>
             <button className='roll-dice'onClick={rollDice}>{tenzies?'New Game':'Roll Dice'}</button>
+            <Timer 
+                time ={time}
+            />
+            <p>{count}</p>
+            
+            
         </main>
     )
 }
